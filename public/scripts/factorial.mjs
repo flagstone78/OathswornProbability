cfaff979917836b8dbad38dc58e351a848b919cd
Alan Mathias <alanjmathias@gmail.com>
2024-04-23 19:32:50 -0600

    Add factorial class to calculate and cache factorials. NOT big int max factorial is 18!

---
+++ public/scripts/factorial.mjs
@@ -0,0 +1,12 @@
+const cache = [1];
+
+function _factorial(n){
+    for(let i=cache.length-1; i<n;i++) cache.push((i+1)*cache[i])
+    return cache[n];
+}
+export function factorial(n){
+    if(n>18) throw "Factorial too large for standard numbers";
+    if(n<0) return Infinity; //throw "Cannot have negative factorial";
+    const val = cache[n];
+    return (val !== undefined) ? val : _factorial(n);
+}
